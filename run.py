02208e91e60170f786f8fc2c42065f1fb06f98aa
abonello <ab@anthonybonello.co.uk>
2018-07-29 18:33:09 +0100

    Add new pieces.

--- run.py
+++ run.py
@@ -39,19 +39,19 @@ def save_book():
             if request.form.get(str(ndx+1)) != None:
                 instruments.append(instrument)
         comment = request.form['comment']
-        pieces = {
-            "pieceTitle":"",
-            "numberInBook":"",
-            "pieceComposer":"",
-            "entries": [
-                {
-                    "instrument": "",
-                    "grade": "",
-                    "syllabusYear": "",
-                    "list": ""
-                }
-            ]
-        }
+        # pieces = {
+        #     "pieceTitle":"",
+        #     "numberInBook":"",
+        #     "pieceComposer":"",
+        #     "entries": [
+        #         {
+        #             "instrument": "",
+        #             "grade": "",
+        #             "syllabusYear": "",
+        #             "list": ""
+        #         }
+        #     ]
+        # }
 
         store=""
         with open("data/books.json", "r") as readdata:
@@ -67,15 +67,17 @@ def save_book():
                                                 "clefs": clefs,
                                                 "grades": grades,
                                                 "instruments": instruments,
-                                                "comment": comment,
-                                                "pieces": pieces
+                                                "comment": comment
                                                 }
         book['indexes'].append(max(book['indexes'])+1)
 
         with open("data/books.json", "w") as outfile:
             json.dump(book, outfile, sort_keys=True, indent=4)
         
-        return "Trying to save this book. {}, {}, {}, {}, {}, {}, clefs:{}, grades: {}, instruments: {}, comment: {}".format(title, volume, bookNo, composer, arranger, publisher, clefs, grades, instruments, comment)
+        currentRoute = "index"
+        message = "Trying to save this book. {}, {}, {}, {}, {}, {}, clefs:{}, grades: {}, instruments: {}, comment: {}".format(title, volume, bookNo, composer, arranger, publisher, clefs, grades, instruments, comment)
+        # return "Trying to save this book. {}, {}, {}, {}, {}, {}, clefs:{}, grades: {}, instruments: {}, comment: {}".format(title, volume, bookNo, composer, arranger, publisher, clefs, grades, instruments, comment)
+        return render_template("index.html", current_route=currentRoute, message=message)
     return "POST was not accepted."
 
 @app.route('/view_books')
@@ -99,17 +101,93 @@ def view_book_details(id):
     # return "This is the detailed view of book with ID:{}. <br>This book is {}".format(id, thisBook)
     return render_template("view_book_details.html", current_route=currentRoute, message="This is the detailed view of book with ID:{}.".format(id), thisBook=thisBook)
 
-@app.route('/add_piece')
-def add_piece():
+@app.route('/add_piece/<id>')
+def add_piece(id):
+    store=""
+    with open("data/books.json", "r") as readdata:
+        store = readdata.read()
+    books = json.loads(store)
+    thisBook = books[id]
     currentRoute = "add_piece"
-    bookInfo = {}
-    bookInfo['title'] = "An Example book"
-    bookInfo['volume'] = "2"
-    bookInfo['book'] = "1"
-    bookInfo['composer'] = "Beethoven"
-    bookInfo['arranger'] = "Someone"
-    bookInfo['publisher'] = "ABRSM"
-    return render_template("add_piece.html", current_route=currentRoute, message="This is the ADD PIECE page of Brass Instruments Books", bookInfo=bookInfo)
+    # bookInfo = {}
+    # bookInfo['title'] = thisBook['title']
+    # bookInfo['volume'] = thisBook['volume']
+    # bookInfo['book'] = thisBook['bookNo']
+    # bookInfo['composer'] = thisBook['composer']
+    # bookInfo['arranger'] = thisBook['arranger']
+    # bookInfo['publisher'] = thisBook['publisher']
+    return render_template("add_piece.html", current_route=currentRoute, message="This is the ADD PIECE page of Brass Instruments Books", thisBook=thisBook)
+    # return render_template("add_piece.html", current_route=currentRoute, message="This is the ADD PIECE page of Brass Instruments Books", bookInfo=bookInfo)
+
+
+@app.route('/save_new_piece/<id>', methods=['GET', 'POST'])
+def save_new_piece(id):
+    print("Received data for book with id: {}".format(id))
+    if request.method == 'POST': 
+        store=""
+        with open("data/books.json", "r") as readdata:
+            store = readdata.read()
+        books = json.loads(store)
+        thisBook = books[id]
+        try:
+            thisBook['pieces']
+            # print(pieces)
+        except:
+            print("There were no pieces set yet")
+            thisBook['pieces'] = []
+            # pieces = thisBook['pieces']
+
+        thisPiece={}
+        thisPiece['pieceTitle'] = request.form['title']
+        thisPiece['numberInBook'] = request.form['number']
+        thisPiece['pieceComposer'] = request.form['composer']
+        thisPiece['entries'] = []
+        entry = {}
+        entry['instrument'] = request.form['instrument']
+        entry['grade'] = request.form['grade']
+        # entry['list'] = request.form['list']
+        if request.form['optradio1']:
+            entry['list'] = 'A'
+        elif request.form['optradio2']:
+            entry['list'] = 'B'
+        elif request.form['optradio3']:
+            entry['list'] = 'C'
+        entry['syllabusYear'] = request.form['syllabusYear']
+        thisPiece['entries'].append(entry)
+        # thisPiece['instrument'] = request.form['instrument']
+        # thisPiece['grade'] = request.form['grade']
+        # thisPiece['list'] = request.form['list']
+        # thisPiece['syllabusYear'] = request.form['syllabusYear']
+
+        # pieces = {
+        #     "pieceTitle":"",
+        #     "numberInBook":"",
+        #     "pieceComposer":"",
+        #     "entries": [
+        #         {
+        #             "instrument": "",
+        #             "grade": "",
+        #             "syllabusYear": "",
+        #             "list": ""
+        #         }
+        #     ]
+        # }
+
+        # print(thisPiece)
+        thisBook['pieces'].append(thisPiece)
+        
+        # print(thisBook)
+        # books[thisBook['id']] = thisBook
+        
+
+        with open("data/books.json", "w") as outfile:
+            json.dump(books, outfile, sort_keys=True, indent=4)
+
+
+
+
+    currentRoute="view_book_details"
+    return render_template("view_book_details.html", current_route=currentRoute, message="", thisBook=thisBook)
 
 
 
